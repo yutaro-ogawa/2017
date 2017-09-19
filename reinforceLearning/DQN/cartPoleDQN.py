f7b5acbb72f7b32866b3f63c5b295afa1baf9dba
Unknown <yutaro.ogawa.1116@gmail.com>
2017-09-19 19:00:40 +0900

    [wip] DQNでcartPole、実装はできたから、あと綺麗にしていく

--- reinforceLearning/DQN/cartPoleDQN.py
+++ reinforceLearning/DQN/cartPoleDQN.py
@@ -21,47 +21,60 @@ class QNetwork:  # [A]Neural Networkを定義
 
 
 class Memory:  # [B]状態と行動を保存するバッファーメモリを定義
-        def __init__(self, max_size=1000):
-            self.buffer = deque(maxlen=max_size)
+    def __init__(self, max_size=1000):
+        self.buffer = deque(maxlen=max_size)
 
-        def add(self, experience):
-            self.buffer.append(experience)
+    def add(self, experience):
+        self.buffer.append(experience)
 
-        def sample(self, batch_size):
-            idx = np.random.choice(np.arange(len(self.buffer)), size=batch_size, replace=False)
-            return [self.buffer[ii] for ii in idx]
+    def sample(self, batch_size):
+        idx = np.random.choice(np.arange(len(self.buffer)), size=batch_size, replace=False)
+        return [self.buffer[ii] for ii in idx]
+
+
+class Actor:
+    def get_action(self, state, episode, mainQN):   # [C]ｔ＋１での行動を返す
+        # 徐々に最適行動のみをとる、ε-greedy法
+        epsilon = 0.5 * (1 / (episode + 1))
+
+        if epsilon <= np.random.uniform(0, 1):
+            retTargetQs = mainQN.model.predict(state)[0]
+            action = np.argmax(retTargetQs)  # 最大の報酬を返す行動を選択する
+
+        else:
+            action = np.random.choice([0, 1])  # ランダムに行動する
+
+        return action
 
 
 # [C] メイン関数開始----------------------------------------------------
 # [D] 初期設定--------------------------------------------------------
 env = gym.make('CartPole-v0')
 max_number_of_steps = 200  # 1試行のstep数
-num_consecutive_iterations = 100  # 学習完了評価に使用する平均試行回数
-num_episodes = 1500  # 総試行回数
+num_consecutive_iterations = 10  # 学習完了評価に使用する平均試行回数
+num_episodes = 1000  # 総試行回数
 goal_average_reward = 195  # この報酬を超えると学習終了（中心への制御なし）
 # goal_average_reward = 150  #【中央に留める用】（中心への制御あり）
 total_reward_vec = np.zeros(num_consecutive_iterations)  # 各試行の報酬を格納
 final_x = np.zeros((num_episodes, 1))  # 学習後、各試行のt=200でのｘの位置を格納
-gamma = 0.99 # 割引係数
+gamma = 0.99    # 割引係数
 islearned = 0  # 学習が終わったフラグ
 isrender = 0  # 描画フラグ
-
-# Network parameters
+# ---
 hidden_size = 16               # Q-networkの隠れ層のニューロンの数
 learning_rate = 0.001         # Q-networkの学習係数
-
-# Memory parameters
 memory_size = 10000            # バッファーメモリの大きさ
-batch_size = 32                # Q-networkを更新するバッチの大記載
+batch_size = 64                # Q-networkを更新するバッチの大記載
 pretrain_length = batch_size   # 開始前に事前に学習してゔバッファーメモリに入れておく量
 
 # [E] ネットワークとメモリの生成--------------------------------------------------------
 mainQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate)
 memory = Memory(max_size=memory_size)
+actor = Actor()
 
 # [F] メモリに初期の状況を保存--------------------------------------------------------
 env.reset()  # cartPoleの環境初期化
-state, reward, done, _ = env.step(env.action_space.sample())  #適当な行動をとる
+state, reward, done, _ = env.step(env.action_space.sample())    # 適当な行動をとる
 state = np.reshape(state, [1, 4])
 
 
@@ -69,81 +82,93 @@ state = np.reshape(state, [1, 4])
 for ii in range(pretrain_length):
     # Uncomment the line below to watch the simulationif done:  # The simulation fails so no next state
     # env.render()    next_state = np.zeros(state.shape)
-    memory.add((state, action, reward, next_state))
     # Make a random action
     action = env.action_space.sample()
-    next_state, reward, done, _ = env.step(action)
-    next_state = np.reshape(next_state, [1, 4])
+    observation, reward, done, _ = env.step(action)
+    next_state = np.reshape(observation, [1, 4])
 
+    if done:
+        # The simulation fails so no next state
+        next_state = np.zeros(state.shape)
+        # Add experience to memory
+        memory.add((state, action, reward, next_state))
 
-# D. t+1の状態から、次の行動a_{t+1}を決定し、Qテーブルを更新する関数 -------------------------------------
-def get_action(state, action, observation, reward, episode):
-    next_state = digitize_state(observation)  # t+1での観測状態を、離散値に変換
-
-    # 徐々に最適行動のみをとる、ε-greedy法
-    epsilon = 0.5 * (1 / (episode + 1))
-    if epsilon <= np.random.uniform(0, 1):
-        next_action = np.argmax(q_table[next_state])
+        # Start new episode
+        env.reset()
+        # Take one random step to get the pole and cart moving
+        observation, reward, done, _ = env.step(env.action_space.sample())
+        state = np.reshape(observation, [1, 4])
     else:
-        next_action = np.random.choice([0, 1])
+        # Add experience to memory
+        memory.add((state, action, reward, next_state))
+        state = next_state
 
-    # Qテーブルの更新
-    if islearned == 0:
-        alpha = 0.2
-        q_table[state, action] = (1 - alpha) * q_table[state, action] + \
-                                 alpha * (reward + gamma * q_table[next_state, next_action])
-
-    return next_action, next_state
-
-
+#############
+# Training
+#############
 # E. メインメソッド--------------------------------------------------
 for episode in range(num_episodes):  # 試行数分繰り返す
-    # 環境の初期化
-    observation = env.reset()
-    state = digitize_state(observation)
-    action = np.argmax(q_table[state])
+    env.reset()  # cartPoleの環境初期化
+    state, reward, done, _ = env.step(env.action_space.sample())  # 適当な行動をとる
+    state = np.reshape(state, [1, 4])
     episode_reward = 0
 
-    for t in range(max_number_of_steps):  # 1試行のループ
+    for t in range(max_number_of_steps + 1):  # 1試行のループ
         if islearned == 1:  # 学習終了したらcartPoleを描画する
             env.render()
             time.sleep(0.1)
-            print(observation[0])  # カートのx位置を出力
+            print(state[0, 0])  # カートのx位置を出力
+        action = actor.get_action(state, episode, mainQN)   # tでの行動を決定する
+        next_state, reward, done, info = env.step(action)   # 行動a_tの実行による、s_{t+1}, _R{t}を計算する
+        next_state = np.reshape(next_state, [1, 4])
 
-        # 行動a_tの実行により、s_{t+1}, r_{t+1}などを計算する
-        observation, reward, done, info = env.step(action)
+        # 中央にとどめる報酬制御
+        if state[0, 0] > 0.3 or state[0, 0] < -0.3:
+            done = 1
 
-        # 報酬を設定し与える
+        # 報酬を設定し、与える
         if done:
+            next_state = np.zeros(state.shape)  # 次の状態はない
             if t < 195:
-                reward = -200  # こけたら罰則
+                reward = -1  # クリッピングで報酬は1, 0, -1に固定
             else:
-                reware = 0  # 立ったまま終了時は罰則はなし
+                reward = 1  # 立ったまま終了時は報酬
         else:
-            reward = 1  # 各ステップで立ってたら報酬追加
+            reward = 1  # 各ステップで立ってたら報酬追加　はじめからrewardに1が入っているが、明示的に表す
 
         episode_reward += reward  # 報酬を追加
 
-        # 中央にとどめる報酬制御
-        if observation[0] > 0.3 or observation[0] < -0.3:
-            reward = -10  # 中心から外れたら罰則を与える
-            # episode_reward += reward #【中央に留める用】中心への制御をする場合はコメント化をはずす
+        memory.add((state, action, reward, next_state)) # メモリと状態の更新する
+        state = next_state
 
-        # 次の行動a_{t+1}、と離散状態s_{t+1}を求め、Q関数も更新する
-        action, state = get_action(state, action, observation, reward, episode)
-
-        # 終了時の処理
-        if done:
+        if done:    # 終了時の処理
+            total_reward_vec = np.hstack((total_reward_vec[1:], episode_reward))  # 報酬を記録
             print('%d Episode finished after %f time steps / mean %f' %
                   (episode, t + 1, total_reward_vec.mean()))
-            total_reward_vec = np.hstack((total_reward_vec[1:],
-                                          episode_reward))  # 報酬を記録
+
             if islearned == 1:  # 学習終わってたら最終のx座標を格納
-                final_x[episode, 0] = observation[0]
+                final_x[episode, 0] = state[0, 0]
             break
 
-    if (total_reward_vec.mean() >=
-            goal_average_reward):  # 直近の100エピソードが規定報酬以上であれば成功
+        # Qネットワークの学習 小川ここを別クラスにしたい、DDQNにもしたい
+        if np.mod(t, 2) == 0:
+            inputs = np.zeros((batch_size, 4))
+            targets = np.zeros((batch_size, 2))
+            mini_batch = memory.sample(batch_size)
+
+            for i, (state_b, action_b, reward_b, next_state_b) in enumerate(mini_batch):
+                inputs[i:i + 1] = state_b
+                target = reward_b
+
+                if not (next_state_b == np.zeros(state_b.shape)).all(axis=1):
+                    target_Q = mainQN.model.predict(next_state_b)[0]
+                    target = reward_b + gamma * np.amax(mainQN.model.predict(next_state_b)[0])
+                targets[i] = mainQN.model.predict(state_b)
+                targets[i][action_b] = target
+            mainQN.model.fit(inputs, targets, epochs=1, verbose=0)  #epochsは訓練データの反復回数、verbose=0は表示なしの設定
+
+    #複数施行の平均報酬で終了を判断
+    if (total_reward_vec.mean() >= goal_average_reward):  # 直近の平均エピソードが規定報酬以上であれば成功
         print('Episode %d train agent successfuly!' % episode)
         islearned = 1
         # np.savetxt('learned_Q_table.csv',q_table, delimiter=",") #Qtableの保存する場合
@@ -159,3 +184,4 @@ for episode in range(num_episodes):  # 試行数分繰り返す
 
 if islearned:
     np.savetxt('final_x.csv', final_x, delimiter=",")
+
