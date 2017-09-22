9280e6bde4fb1a5bb9848dd32710b3682be6cf2e
Unknown <yutaro.ogawa.1116@gmail.com>
2017-09-22 12:51:24 +0900

    [COMP] DQN, DDQN: 損失関数をsmooth hingeに

--- reinforceLearning/DQN/cartPoleDQN.py
+++ reinforceLearning/DQN/cartPoleDQN.py
@@ -6,8 +6,21 @@ import time
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.utils import plot_model
 from collections import deque
 # from gym import wrappers  # gymの画像保存
+
+from keras import backend as K
+
+
+# 損失関数に勾配クリップしたhuber関数を使用します
+def huberloss(y_true, y_pred):
+    dif=K.abs(y_pred-y_true)
+    error=dif+K.log(1.0+K.exp(-2.0*dif))
+
+    return K.mean(error, axis=1)
+    # return K.mean((1.0/2*K.square(y_pred-y_true)), axis=1)
+    # return K.mean(K.minimum(1.0/2*K.square(y_pred-y_true), K.abs(y_pred-y_true)), axis=1)
 
 
 # [2]Q関数をディープラーニングのネットワークをクラスとして定義
@@ -17,8 +30,9 @@ class QNetwork:
         self.model.add(Dense(hidden_size, activation='relu', input_dim=state_size))
         self.model.add(Dense(hidden_size, activation='relu'))
         self.model.add(Dense(action_size, activation='linear'))
-        self.optimizer = Adam(lr=learning_rate)
-        self.model.compile(loss='mse', optimizer=self.optimizer)
+        self.optimizer = Adam(lr=learning_rate)  # 誤差を減らす学習方法はAdamとし、勾配は最大1にクリップする
+        # self.model.compile(loss='mse', optimizer=self.optimizer)
+        self.model.compile(loss=huberloss, optimizer=self.optimizer)
 
     # 重みの学習
     def replay(self, memory, batch_size, gamma, targetQN):
@@ -59,7 +73,7 @@ class Memory:
 class Actor:
     def get_action(self, state, episode, targetQN):   # [C]ｔ＋１での行動を返す
         # 徐々に最適行動のみをとる、ε-greedy法
-        epsilon = 0.005 + 0.5 * np.exp(-0.01 * episode)
+        epsilon = 0.001 + 0.9 / (1.0+episode)
 
         if epsilon <= np.random.uniform(0, 1):
             retTargetQs = targetQN.model.predict(state)[0]
@@ -87,13 +101,17 @@ islearned = 0  # 学習が終わったフラグ
 isrender = 0  # 描画フラグ
 # ---
 hidden_size = 16               # Q-networkの隠れ層のニューロンの数
-learning_rate = 0.001         # Q-networkの学習係数
+#learning_rate = 0.0001         # Q-networkの学習係数
+
+learning_rate = 0.00001         # Q-networkの学習係数
+
 memory_size = 10000            # バッファーメモリの大きさ
 batch_size = 32                # Q-networkを更新するバッチの大記載
 
 # [4.2]Qネットワークとメモリ、Actorの生成--------------------------------------------------------
 mainQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate)     # 価値を計算するQネットワーク
 targetQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate)   # 行動を決定するQネットワーク
+# plot_model(mainQN, to_file='Qnetwork.png', show_shapes=True)        # Qネットワークの可視化
 memory = Memory(max_size=memory_size)
 actor = Actor()
 
@@ -123,7 +141,7 @@ for episode in range(num_episodes):  # 試行数分繰り返す
 
         # 報酬を設定し、与える
         if done:
-            next_state = np.zeros(state.shape)  # 次の状態s_{t+1}はない
+            next_state = np.dzeros(state.shape)  # 次の状態s_{t+1}はない
             if t < 195:
                 reward = -1  # 報酬クリッピング、報酬は1, 0, -1に固定
             else:
@@ -131,7 +149,7 @@ for episode in range(num_episodes):  # 試行数分繰り返す
         else:
             reward = 0  # 各ステップで立ってたら報酬追加（はじめからrewardに1が入っているが、明示的に表す）
 
-        episode_reward += reward  # 合計報酬を更新
+        episode_reward += 1 # reward  # 合計報酬を更新
 
         memory.add((state, action, reward, next_state))     # メモリの更新する
         state = next_state  # 状態更新
